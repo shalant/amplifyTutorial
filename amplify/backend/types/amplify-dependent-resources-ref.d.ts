3b9711336bad6a7babdc2f8e1cdbdceafb778bbb
shalant <doug.rosenberg@gmail.com>
2022-06-02 21:52:02 -0500

    dead end

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,8 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "amplifytutorial": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
